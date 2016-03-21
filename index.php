7e7b2873a31a7f958bf4500c9cf75b929bb5c529
hikalium <hikalium@hikalium.com>
2016-03-21 14:43:24 +0900

    授業テーブルのソートに対応。色付けと単位統計情報も追加。

--- index.php
+++ index.php
@@ -39,15 +39,72 @@ function showTimeTable($maxPeriod = 6){
 table {
 	table-layout: fixed;
 }
+
+.kk-A1 {
+	background-color: #ff5852;
+}
+.kk-A2 {
+	background-color: #ffa8c7;
+}
+.kk-B1 {
+	background-color: #a1beff;
+}
+.kk-B2 {
+	background-color: #c7ff7d;
+}
+.kk-B3 {
+	background-color: #74ff4f;
+}
+.kk-B4 {
+	background-color: #39beff;
+}
+.kk-C {
+	background-color: #fff1b1;
+}
+.kk-D {
+	background-color: #9489ff;
+}
+.kk-A1 {
+	background-color: #ff5852;
+}
+.kk-A1 {
+	background-color: #ff5852;
+}
 	</style>
 	<script>
 
 var focusDay = -1, focusPeriod = -1, focusElemSel, focusElemOldCSS;
 var focusListSel, focusListOldCSS;
 var timeTableSel;
+var statTableAreaSel;
+var timeTableCells = [[], [], [], [], [], [], []];
 var currentFocusLabelSel;
 var candidateClassResultAreaSel;
 var youbiList = ["日", "月", "火", "水", "木", "金", "土"];
+var kamokuKubunList = {
+	'綜合科目 選択必修': 		'A1',
+	'特論科目 選択必修': 		'A1',
+	'基礎科目 選択必修': 		'A1',
+	'数学　必修':				'B1',
+	'実験・実習・制作':			'B3',
+	'専門必修':					'C',
+	'領域コース 必修':			'A1',
+	'専門選択必修':				'C',
+	'外国語　英語　必修':		'A2',
+	'外国語　英語　選択必修':	'A2',
+	'外国語　英語　選択':		'A2',
+	'実験・実習・制作　必修':	'B3',
+	'情報関連科目　選択':		'B4',
+	'自主挑戦科目':				'D',
+	'専門選択':					'C',
+	'綜合科目 選択':			'A1',
+	'自然科学　物理学　必修':	'B2',
+	'自然科学　物理学　選択':	'B2',
+	'自然科学　化学　必修':		'B2',
+	'自然科学　生命科学　必修':	'B2',
+	'情報関連科目　必修':		'B4',
+};
+var takingClassCodeList = new Array();
 
 <?php
 	$str = file_get_contents("./data/2016_kikan_index.json");
@@ -80,13 +137,12 @@ function moveCLFocus(elem, code){
 	focusListOldCSS = focusListSel.css("border");
 	focusListSel.css("border", "2px solid #ff0000");
 	//
-	var c = classList[code];
-	focusElemSel.text(c[4]);
-	localStorage[focusDay + "_" + focusPeriod] = code;
+	takeClass(code);
 }
 
 function showCandidateClassTable(d, p){
 	var table = $('<table>')
+		.addClass("tablesorter")
 		.addClass("table")
 		.addClass("table-bordered");
 	var th = $('<tr>')
@@ -114,33 +170,129 @@ function showCandidateClassTable(d, p){
 	//console.log(table);
 	//console.log(candidateClassResultAreaSel);
 	candidateClassResultAreaSel.empty().append(table);
+	table.tablesorter(); 
 } 
 
 function erasePeriod(){
+	var code = timeTableCells[focusDay][focusPeriod].classCode;
+	var c = classList[code];
+	if(!c){
+		console.log("Invalid classcode.")
+		return;
+	}
+	var pList = c[0];
+	for(var i = 0; i < pList.length; i++){
+		timeTableCells[pList[i][0]][pList[i][1]].text("");
+		timeTableCells[pList[i][0]][pList[i][1]].classCode = null;
+		if(kamokuKubunList[c[5]]){
+			timeTableCells[pList[i][0]][pList[i][1]].removeClass();
+		}
+	}
 	focusElemSel.text("");
-	localStorage.removeItem(focusDay + "_" + focusPeriod);
+	takingClassCodeList.removeAnObject(code);
+	localStorage["defaultTable"] = JSON.stringify(takingClassCodeList);
+	updateStatistics();
+}
+
+function takeClass(code){
+	var c = classList[code];
+	if(!c){
+		console.log("Invalid classcode.")
+		return;
+	}
+	var pList = c[0];
+	for(var i = 0; i < pList.length; i++){
+		timeTableCells[pList[i][0]][pList[i][1]].text(c[4]);
+		timeTableCells[pList[i][0]][pList[i][1]].classCode = code;
+		if(kamokuKubunList[c[5]]){
+			timeTableCells[pList[i][0]][pList[i][1]].addClass("kk-" + kamokuKubunList[c[5]]);
+		}
+	}
+	takingClassCodeList.pushUnique(code);
+	localStorage["defaultTable"] = JSON.stringify(takingClassCodeList);
+	updateStatistics();
+}
+
+function updateStatistics(){
+	var stat = {
+		"A1":0,
+		"A2":0,
+		"B1":0,
+		"B2":0,
+		"B3":0,
+		"B4":0,
+		"C":0,
+		"D":0,
+		"sum":0
+	};
+	for(var i = 0; i < takingClassCodeList.length; i++){
+		var code = takingClassCodeList[i];
+		var c = classList[code];
+		if(!c){
+			continue;
+		}
+		if(kamokuKubunList[c[5]]){
+			stat[kamokuKubunList[c[5]]] += c[2];
+			stat["sum"] += c[2];
+		} else{
+			console.log("Unknown kamokukubun " + c[5]);
+		}
+	}
+	
+	//
+	var table = $('<table>')
+		.addClass("table")
+		.addClass("table-bordered");
+	var th = $('<tr>')
+		.append($('<th>').text("A1").addClass("kk-A1"))
+		.append($('<th>').text("A2").addClass("kk-A2"))
+		.append($('<th>').text("B1").addClass("kk-B1"))
+		.append($('<th>').text("B2").addClass("kk-B2"))
+		.append($('<th>').text("B3").addClass("kk-B3"))
+		.append($('<th>').text("B4").addClass("kk-B4"))
+		.append($('<th>').text("C").addClass("kk-C"))
+		.append($('<th>').text("D").addClass("kk-D"))
+		.append($('<th>').text("計"));
+	table.append($('<thead>').append(th));
+	var tbody = $('<tbody>');
+	var td = $('<tr>')
+		.append($('<td>').text(stat["A1"]))
+		.append($('<td>').text(stat["A2"]))
+		.append($('<td>').text(stat["B1"]))
+		.append($('<td>').text(stat["B2"]))
+		.append($('<td>').text(stat["B3"]))
+		.append($('<td>').text(stat["B4"]))
+		.append($('<td>').text(stat["C"]))
+		.append($('<td>').text(stat["D"]))
+		.append($('<td>').text(stat["sum"]));
+	tbody.append(td);
+	table.append(tbody);
+	//
+	statTableAreaSel.empty().append(table);
 }
 
 onload = function(){
 	currentFocusLabelSel = $("#currentFocusLabel");
 	candidateClassResultAreaSel = $("#candidateClassResultArea");
 	timeTableSel = $("#timeTable");
+	statTableAreaSel = $("#statTableArea");
 	//
 	var rowList = timeTableSel[0].children[1].children;
 	for(var p = 0; p < rowList.length; p++){
 		var days = rowList[p].children;
-		//console.log(days);
 		for(d = 1; d < days.length; d++){
-			var t = days[d];
-			var code = localStorage[d + "_" + (p + 1)];
-			//console.log(code);
-			if(code){
-				var c = classList[code];
-				if(c){
-					$(t).text(c[4]);
-				}
-			}
+			var t = days[d];;
+			timeTableCells[d][p + 1] = $(t);
 		}
+	}
+	//
+	if(localStorage["defaultTable"]){
+		takingClassCodeList = JSON.parse(localStorage["defaultTable"]);
+	} else{
+		takingClassCodeList = new Array();
+	}
+	for(var i = 0; i < takingClassCodeList.length; i++){
+		takeClass(takingClassCodeList[i]);
 	}
 }
 
@@ -154,13 +306,16 @@ onload = function(){
 	<div class="container">
 		<h1>WTT</h1>
 		<h2>現在の時間割</h2>
-		<?php showTimeTable(); ?>
 		<button onclick="erasePeriod()"><span class="glyphicon glyphicon-erase"></span></button>
+		<?php showTimeTable(); ?>
+		<div id="statTableArea"></div>
 		<h2>選択可能な授業</h2>
 		<h4 id="currentFocusLabel">編集する時限を上から選択してください</h4>
 		<div id="candidateClassResultArea"></div>
 	</div>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./js/jquery.tablesorter.js"></script>
+	<script type="text/javascript" src="./js/ext.js"></script>
 </body>
 </html>
